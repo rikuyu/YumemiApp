bd6d529b24fa1a977ce9ed65a0cf6f3258a42560
rikuyu <rikuyu5884@gmail.com>
2021-08-29 00:33:13 +0900

    create api, di module

---
+++ app/src/main/java/com/example/yumemiapp/di/NetworkModule.kt
@@ -0,0 +1,26 @@
+package com.example.yumemiapp.di
+
+import com.example.yumemiapp.model.api.YumemiApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    private const val BASE_URL = "https://api.github.com/repositories/"
+
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance(): YumemiApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(YumemiApi::class.java)
+    }
+}
